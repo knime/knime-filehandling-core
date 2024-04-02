9738fc75f2c5960d7a691167ce863e99cdd68f39
Paul Bärnreuther <paul.baernreuther@tngtech.com>
2024-04-02 09:17:44 +0200

    UIEXT-1499: Split accessor logic from node model settings
    UIEXT-1499 (Add autodetect button)

--- org.knime.filehandling.core/src/org/knime/filehandling/core/defaultnodesettings/filechooser/FileChooserPathAccessor.java
+++ org.knime.filehandling.core/src/org/knime/filehandling/core/defaultnodesettings/filechooser/FileChooserPathAccessor.java
@@ -48,37 +48,11 @@
  */
 package org.knime.filehandling.core.defaultnodesettings.filechooser;
 
-import java.io.IOException;
-import java.nio.file.FileVisitOption;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Consumer;
 
-import org.knime.core.node.InvalidSettingsException;
-import org.knime.core.node.util.CheckUtils;
 import org.knime.filehandling.core.connections.FSConnection;
-import org.knime.filehandling.core.connections.FSFileSystem;
-import org.knime.filehandling.core.connections.FSFiles;
-import org.knime.filehandling.core.connections.FSLocation;
 import org.knime.filehandling.core.connections.FSPath;
-import org.knime.filehandling.core.defaultnodesettings.ExceptionUtil;
-import org.knime.filehandling.core.defaultnodesettings.FileSystemHelper;
-import org.knime.filehandling.core.defaultnodesettings.ValidationUtils;
-import org.knime.filehandling.core.defaultnodesettings.filechooser.reader.FileFilterStatistic;
-import org.knime.filehandling.core.defaultnodesettings.filechooser.reader.ReadPathAccessor;
-import org.knime.filehandling.core.defaultnodesettings.filechooser.writer.WritePathAccessor;
-import org.knime.filehandling.core.defaultnodesettings.filtermode.FileAndFolderFilter;
-import org.knime.filehandling.core.defaultnodesettings.filtermode.SettingsModelFilterMode;
-import org.knime.filehandling.core.defaultnodesettings.filtermode.SettingsModelFilterMode.FilterMode;
-import org.knime.filehandling.core.defaultnodesettings.status.DefaultStatusMessage;
-import org.knime.filehandling.core.defaultnodesettings.status.StatusMessage;
-import org.knime.filehandling.core.defaultnodesettings.status.StatusMessage.MessageType;
 
 /**
  * Allows access to the {@link FSPath FSPaths} referred to by the {@link AbstractSettingsModelFileChooser} provided in
@@ -89,28 +63,7 @@ import org.knime.filehandling.core.defaultnodesettings.status.StatusMessage.Mess
  * @noreference non-public API
  * @noinstantiate non-public API
  */
-public final class FileChooserPathAccessor implements ReadPathAccessor, WritePathAccessor {
-
-    /**
-     * The root location i.e. the location to start scanning the file tree from
-     */
-    private final FSLocation m_rootLocation;
-
-    private final Optional<FSConnection> m_portObjectConnection;
-
-    private final AbstractSettingsModelFileChooser<?> m_settings;
-
-    private final FilterMode m_filterMode;
-
-    private FileFilterStatistic m_fileFilterStatistic;
-
-    /**
-     * The connection used to create the {@link FSFileSystem} used to create the {@link FSPath paths}.</br>
-     * If m_rootLocation isn't empty, this will be initialized with m_rootLocation.get().
-     */
-    private FSConnection m_connection;
-
-    private FSFileSystem<?> m_fileSystem;
+public final class FileChooserPathAccessor extends AbstractFileChooserPathAccessor {
 
     /**
      * Creates a new FileChooserAccessor for the provided {@link AbstractSettingsModelFileChooser} and
@@ -123,182 +76,10 @@ public final class FileChooserPathAccessor implements ReadPathAccessor, WritePat
      */
     public FileChooserPathAccessor(final AbstractSettingsModelFileChooser<?> settings,
         final Optional<FSConnection> portObjectConnection) { //NOSONAR
-        m_rootLocation = settings.getLocation();
-        m_portObjectConnection = portObjectConnection;
-        m_settings = settings;
-        m_filterMode = m_settings.getFilterModeModel().getFilterMode();
-    }
-
-    private FSConnection getConnection() {
-        if (m_portObjectConnection.isPresent()) {
-            // if we have a file system port, we always use the provided connection
-            return m_portObjectConnection.get();
-        } else {
-            // otherwise we retrieve the connection from the location
-            return FileSystemHelper.retrieveFSConnection(m_portObjectConnection, m_rootLocation).orElseThrow(
-                () -> new IllegalStateException("No file system connection available. Execute connector node."));
-        }
-    }
-
-    private void initializeFileSystem() {
-        if (m_connection == null) {
-            m_connection = getConnection();
-            m_fileSystem = m_connection.getFileSystem();
-        }
-    }
-
-    /**
-     * Retrieves the output/root {@link FSPath} specified in the settings provided to the constructor.</br>
-     * Writer nodes should make use of this method.
-     *
-     * @param statusMessageConsumer used to communicating non-fatal erros and warnings
-     * @return the output path
-     * @throws InvalidSettingsException if the settings provided in the constructor are invalid
-     */
-    @Override
-    public FSPath getOutputPath(final Consumer<StatusMessage> statusMessageConsumer) throws InvalidSettingsException {
-        CheckUtils.checkArgumentNotNull(statusMessageConsumer, "The statusMessageConsumer must not be null.");
-
-        initializeFileSystem();
-
-        try {
-            m_fileSystem.checkCompatibility(m_rootLocation);
-        } catch (Exception ex) { // NOSONAR
-            statusMessageConsumer.accept(new DefaultStatusMessage(MessageType.ERROR, ex.getMessage()));
-        }
-
-        if (m_portObjectConnection.isPresent()) {
-            // if present the port object fs always takes precedence
-            return m_fileSystem.getPath(m_rootLocation);
-        } else {
-            return getPathFromConvenienceFs();
-        }
-    }
-
-    private FSPath getPathFromConvenienceFs() throws InvalidSettingsException {
-        switch (m_rootLocation.getFSCategory()) {
-            case CONNECTED:
-                throw new IllegalStateException("The file system is not connected.");
-            case CUSTOM_URL:
-                ValidationUtils.validateCustomURLLocation(m_rootLocation);
-                return m_fileSystem.getPath(m_rootLocation);
-            case RELATIVE:
-                final FSPath path = m_fileSystem.getPath(m_rootLocation);
-                ValidationUtils.validateKnimeFSPath(path);
-                return path;
-            case MOUNTPOINT:
-                return m_fileSystem.getPath(m_rootLocation);
-            case LOCAL:
-                ValidationUtils.validateLocalFsAccess();
-                return m_fileSystem.getPath(m_rootLocation);
-            case HUB_SPACE:
-                return m_fileSystem.getPath(m_rootLocation);
-            default:
-                throw new IllegalStateException("Unsupported file system category: " + m_rootLocation.getFSCategory());
-        }
-    }
-
-    @Override
-    public final List<FSPath> getFSPaths(final Consumer<StatusMessage> statusMessageConsumer)
-        throws IOException, InvalidSettingsException {
-        final FSPath rootPath = getRootPath(statusMessageConsumer);
-
-        if (m_filterMode == FilterMode.FILE || m_filterMode == FilterMode.FOLDER
-            || m_filterMode == FilterMode.WORKFLOW) {
-            return handleSinglePath(rootPath);
-        } else {
-            List<FSPath> fsPaths = walkFileTree(rootPath);
-            FSFiles.sortPathsLexicographically(fsPaths);
-            return fsPaths;
-        }
-    }
-
-    private List<FSPath> handleSinglePath(final FSPath rootPath) throws IOException, InvalidSettingsException {
-        final BasicFileAttributes attr = Files.readAttributes(rootPath, BasicFileAttributes.class);
-        if (m_filterMode == FilterMode.FILE || m_filterMode == FilterMode.WORKFLOW) {
-            String workflowOrFile = m_filterMode.getTextLabel();
-            CheckUtils.checkSetting(!rootPath.toString().trim().isEmpty(), "Please specify a " + workflowOrFile + ".");
-            CheckUtils.checkSetting(!attr.isDirectory(), "%s is a folder. Please specify a " + workflowOrFile + ".",
-                rootPath);
-            m_fileFilterStatistic = new FileFilterStatistic(0, 0, 0, 1, 0, 0, 0);
-        } else if (m_filterMode == FilterMode.FOLDER) {
-            checkIsFolder(rootPath, attr);
-            m_fileFilterStatistic = new FileFilterStatistic(0, 0, 0, 0, 0, 0, 1);
-        } else {
-            throw new IllegalStateException("Unexpected filter mode in handleSingleCase: " + m_filterMode);
-        }
-        return Collections.singletonList(rootPath);
-    }
-
-    @Override
-    public FileFilterStatistic getFileFilterStatistic() {
-        CheckUtils.checkState(m_fileFilterStatistic != null,
-            "No statistic available. Call getFSPaths() or getPaths() first.");
-        return m_fileFilterStatistic;
-    }
-
-    private List<FSPath> walkFileTree(final Path rootPath) throws IOException, InvalidSettingsException {
-        final BasicFileAttributes attrs = Files.readAttributes(rootPath, BasicFileAttributes.class);
-        checkIsFolder(rootPath, attrs);
-        final FilterVisitor visitor = createVisitor(rootPath);
-        final boolean includeSubfolders = m_settings.getFilterModeModel().isIncludeSubfolders();
-        final boolean followLinks = m_settings.getFilterModeModel().isFollowLinks();
-        final Set<FileVisitOption> linkOptions =
-            followLinks ? EnumSet.of(FileVisitOption.FOLLOW_LINKS) : EnumSet.noneOf(FileVisitOption.class);
-        Files.walkFileTree(rootPath, linkOptions, includeSubfolders ? Integer.MAX_VALUE : 1, visitor);
-        m_fileFilterStatistic = visitor.getFileFilterStatistic();
-        final List<?> paths = visitor.getPaths();
-        @SuppressWarnings("unchecked") // we know it better
-        final List<FSPath> fsPaths = (List<FSPath>)paths;
-        return fsPaths;
-    }
-
-    private static void checkIsFolder(final Path rootPath, final BasicFileAttributes attrs)
-        throws InvalidSettingsException {
-        CheckUtils.checkSetting(attrs.isDirectory(), "%s is not a folder. Please specify a folder.", rootPath);
-    }
-
-    private FilterVisitor createVisitor(final Path rootPath) {
-        final SettingsModelFilterMode settings = m_settings.getFilterModeModel();
-        final boolean includeSubfolders = settings.isIncludeSubfolders();
-        final FileAndFolderFilter filter = new FileAndFolderFilter(rootPath, settings.getFilterOptionsSettings());
-        switch (m_filterMode) {
-            case FILES_AND_FOLDERS:
-                return new FilterVisitor(filter, true, true, includeSubfolders);
-            case FILES_IN_FOLDERS:
-                return new FilterVisitor(filter, true, false, includeSubfolders);
-            case FOLDERS:
-                return new FilterVisitor(filter, false, true, includeSubfolders);
-            case FOLDER:
-            case FILE:
-                throw new IllegalStateException(
-                    "Encountered file/folder filter mode when walking the file tree. This is a coding error.");
-            default:
-                throw new IllegalStateException("Unknown filter mode: " + m_filterMode);
-        }
-    }
-
-    @Override
-    public void close() throws IOException {
-        if (m_fileSystem != null) {
-            m_fileSystem.close();
-            m_connection.close();
-        }
-    }
-
-    @Override
-    public FSPath getRootPath(final Consumer<StatusMessage> statusMessageConsumer)
-        throws IOException, InvalidSettingsException {
-        final String errorSuffix = m_filterMode.getTextLabel();
-        CheckUtils.checkSetting(!m_rootLocation.getPath().trim().isEmpty(),
-            String.format(AbstractSettingsModelFileChooser.NO_LOCATION_ERROR, errorSuffix));
-        final FSPath rootPath = getOutputPath(statusMessageConsumer);
-
-        CheckUtils.checkSetting(FSFiles.exists(rootPath), "The specified %s %s does not exist.", errorSuffix, rootPath);
-        if (!Files.isReadable(rootPath)) {
-            throw ExceptionUtil.createAccessDeniedException(rootPath);
-        }
-        return rootPath;
+        super(new FileChooserPathAccessorSettings(settings.getLocation(), new FilterSettings(
+            settings.getFilterModeModel().getFilterMode(), settings.getFilterModeModel().isIncludeSubfolders(),
+            settings.getFilterModeModel().getFilterOptionsSettings(), settings.getFilterModeModel().isFollowLinks())),
+            portObjectConnection);
     }
 
 }
